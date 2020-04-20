c6dcbeb2ad6317c0f244ce038e1190d1e0dd470e
Fox <hwq8910@163.com>
2020-04-20 09:26:53 +0800

    channel

---
+++ test/channel/channelDemo2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Cat struct {
+	Name string
+	Age	int
+}
+
+func main() {
+
+	var allChan chan interface{}
+	allChan = make(chan interface{},3)
+
+	allChan<- 10
+	allChan<- "jack"
+	cat1 := Cat{Name:"小花猫",Age:18,}
+	allChan<- cat1
+
+
+	//取出
+	<- allChan
+	<- allChan
+	newCat := <- allChan
+	fmt.Printf("newCat=%T,newCat=%v",newCat,newCat)
+	a :=newCat.(Cat)//类型断言
+	fmt.Printf("newCat.Name=%v",a.Name)
+}
