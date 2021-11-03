613c3c1ddc7a212caba3188235ec8b7c45e42ee9
timmy.hu <hwq8910@163.com>
2021-11-03 23:48:27 +0800

    gjson demo

---
+++ goframe/gjson_test.go
@@ -0,0 +1,62 @@
+package gostrset
+
+import (
+	"fmt"
+	"github.com/gogf/gf/encoding/gjson"
+	"testing"
+)
+
+func TestJsonToStruct(t *testing.T) {
+	data :=
+		`
+{
+    "count" : 1,
+    "array" : ["John", "Ming"]
+}`
+	type Users struct {
+		Count int
+		Array []string
+	}
+
+	if j, err := gjson.DecodeToJson(data); err != nil {
+		panic(err)
+	} else {
+		users := new(Users)
+		if err := j.Struct(users); err != nil {
+			panic(err)
+		}
+		fmt.Printf(`%+v`, users)
+	}
+}
+
+func TestJsonToStruct1(t *testing.T) {
+	data :=
+		`{
+    "users" : {
+        "count" : 1,
+        "array" : ["John", "Ming"]
+    },
+	"students" : {
+        "name" : "fox",
+        "age" : "10"
+    }
+}`
+	if j, err := gjson.DecodeToJson(data); err != nil {
+		panic(err)
+	} else {
+		type Users struct {
+			Count int
+			Array []string
+		}
+		type Students struct {
+			Name string
+			Age  string
+		}
+		//users := new(Users)
+		students := new(Students)
+		if err := j.GetStruct("students", students); err != nil {
+			panic(err)
+		}
+		fmt.Printf(`%+v`, students)
+	}
+}
