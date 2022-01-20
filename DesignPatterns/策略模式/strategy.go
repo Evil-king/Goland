30f78be651d08cf7774a2b4dd7ff2a170f5cb293
timmy.hu <timmy.hu@lianwei.com.cn>
2022-01-20 15:41:18 +0800

    update

--- DesignPatterns/策略模式/strategy.go
+++ DesignPatterns/策略模式/strategy.go
@@ -5,13 +5,21 @@ import "fmt"
 // 策略模式 定义一系列算法，让这些算法在运行时可以互换，使得分离算法，符号开闭原则。
 
 type Payment struct {
-	context  *PaymentContext
-	strategy PaymentStrategy
+	context  *PaymentContext //支付内容属性
+	strategy PaymentStrategy //支付的策略
+}
+
+func (p *Payment) Pay() {
+	p.strategy.Pay(p.context) // payment的方法通过策略去调用自己的pay方法
 }
 
 type PaymentContext struct {
 	Name, CardID string
 	Money        int
+}
+
+type PaymentStrategy interface {
+	Pay(*PaymentContext)
 }
 
 func NewPayment(name, cardId string, money int, strategy PaymentStrategy) *Payment {
@@ -23,14 +31,6 @@ func NewPayment(name, cardId string, money int, strategy PaymentStrategy) *Payme
 		},
 		strategy: strategy,
 	}
-}
-
-func (p *Payment) Pay() {
-	p.strategy.Pay(p.context)
-}
-
-type PaymentStrategy interface {
-	Pay(*PaymentContext)
 }
 
 type Cash struct{}
